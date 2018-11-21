f81912e598431798e08fec48e3b8684082a91d5a
Daniel Milnes <thebeanogamer@gmail.com>
2018-11-21 11:39:20 +0000

    Make it all work and not bad

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const chalk = require('chalk');
 const clear = require('clear');
-const clui = require('clui');
 const figlet = require('figlet');
-const inquirer   = require('inquirer');
-const menu = require('inquirer-menu');
+const inquirer = require('inquirer');
+var Table = require('cli-table');
 
 var running = true;
 var toExecute = "";
 
 clear();
-const drawMenu = function() {console.log(figlet.textSync('Linked List', { horizontalLayout: 'full' }));};
+const drawTitle = function() {
+  console.log(figlet.textSync('Linked List', {
+    horizontalLayout: 'full'
+  }));
+};
+
+var linkedList = [{"indexPointer": 0, "dataValues": [], "pointer": []}];
 
 const add = function(value) {
   console.log("added");
@@ -30,3 +35,40 @@ const search = function() {
   console.log("searched");
   // TODO: Write search function
 }
+
+drawTitle();
+
+const questions = [
+    { type: 'input', name: 'command', message: 'Please select a command (add/remove/view/search/exit): '},
+];
+
+const mainMenu = async function() {
+  await inquirer
+      .prompt(questions)
+      .then(function (answers) {
+          switch (answers.command.toLowerCase()) {
+            case "add" :
+              add();
+              break;
+            case "remove":
+              remove();
+              break;
+            case "view":
+              view();
+              break;
+            case "search":
+              search();
+              break;
+            case "exit":
+              process.exit();
+          }
+  })
+}
+
+const main = async function() {
+  while (running) {
+    await mainMenu()
+  }
+}
+
+main();
