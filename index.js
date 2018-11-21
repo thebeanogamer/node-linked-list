223bf7794b86a431ffd6c205d8c30f4317c9509c
Daniel Milnes <thebeanogamer@gmail.com>
2018-11-21 09:09:05 +0000

    Switch to Menu Library

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const clear = require('clear');
 const clui = require('clui');
 const figlet = require('figlet');
 const inquirer   = require('inquirer');
+const menu = require('inquirer-menu');
 
 var running = true;
 var toExecute = "";
 
 clear();
-console.log(figlet.textSync('Linked List', { horizontalLayout: 'full' }));
+const drawMenu = function() {console.log(figlet.textSync('Linked List', { horizontalLayout: 'full' }));};
 
 const add = function(value) {
   console.log("added");
@@ -29,36 +30,3 @@ const search = function() {
   console.log("searched");
   // TODO: Write search function
 }
-
-const command = [
-  {
-    name: 'command',
-    type: 'input',
-    message: 'Please input a command (add/remove/view/search):',
-    validate: function( value ) {
-      if (value.length && (value.toLowerCase() == "add") || (value.toLowerCase() == "remove") || (value.toLowerCase() == "view") || (value.toLowerCase() == "search")) {
-        return value;
-      } else {
-        return 'Please enter a valid command.';
-      }
-    }
-  },
-];
-
-const menu = async function() {
-  while (running == true) {
-    toExecute = await inquirer.prompt(command);
-    switch (toExecute) {
-      case "add":
-        add()
-      case "remove":
-        remove()
-      case "view":
-        view()
-      case "search":
-        search()
-  }
-}
-};
-
-menu();
